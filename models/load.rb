bad774a0fac09624c322c5c7fd0a580350680ac0
key <key@key-no-MacBook-Pro.local>
2014-11-03 00:32:57 +0900

    設定ミス修正

--- models/load.rb
+++ models/load.rb
@@ -1,6 +1,6 @@
 require 'mongoid'
-Mongoid.load!('./config/mongoid.yml', :development)
-# Mongoid.load!('./config/mongoid.yml')
+# Mongoid.load!('./config/mongoid.yml', :development)
+Mongoid.load!('./config/mongoid.yml')
 
 require './models/user'
 require './models/body_weight'
